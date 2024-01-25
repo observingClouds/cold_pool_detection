2551e6c81423305c02a8342512d562b8a005a7e1
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-01-24 17:59:38 -0800

    read several granules and combine them to one ds

--- src/data/regrid_SAR.py
+++ src/data/regrid_SAR.py
@@ -1,39 +1,64 @@
 """Regrid irregularly spaced SAR data to regular grid."""
 
+import glob
+
 import numpy as np
 import xarray as xr
 from pyresample.geometry import AreaDefinition, GridDefinition
 from pyresample.kd_tree import XArrayResamplerNN
 
-ds = xr.open_dataset(
-    "../../data/SAR/S1A_IW_OCN__2SDV_20200113T094154_20200113T094223_030780_0387B2_6706.SAFE/measurement/s1a-iw-ocn-vv-20200113t094155-20200113t094228-030780-0387B2-001.nc"
-)
-lon, lat = (ds.owiLon, ds.owiLat)
-lon_out = np.arange(-60.25, -45, 0.01)
-lat_out = np.arange(7.5, 17, 0.01)
 
-area_id = "barbados_east"
-description = "ICON DOM01"
-proj_id = "icon_dom01"
-projection = "EPSG:4326"
+def output_area(lat_min, lat_max, lon_min, lon_max, resolution):
+    """Return output area."""
+    area_id = "barbados_east"
+    description = "ICON DOM01"
+    proj_id = "icon_dom01"
+    projection = "EPSG:4326"
 
-lon_out = np.arange(-60.25, -45, 0.01)
-lat_out = np.arange(7.5, 17, 0.01)
-width = len(lon_out)
-height = len(lat_out)
-area_extent = (-60.25, 7.5, -45, 17)
+    lon_out = np.arange(lon_min, lon_max, resolution)
+    lat_out = np.arange(lat_min, lat_max, resolution)
+    width = len(lon_out)
+    height = len(lat_out)
+    area_extent = (lon_min, lat_min, lon_max, lat_max)
 
-area_def = AreaDefinition(
-    area_id, description, proj_id, projection, width, height, area_extent
-)
-grid_def = GridDefinition(lons=lon.values, lats=lat.values)
+    return AreaDefinition(
+        area_id, description, proj_id, projection, width, height, area_extent
+    )
 
-resampler = XArrayResamplerNN(grid_def, area_def, radius_of_influence=5000)
-resampler.get_neighbour_info()
-result = resampler.get_sample_from_neighbour_info(
-    ds.owiWindSpeed.rename({"owiAzSize": "y", "owiRaSize": "x"})
-)
 
-ds_grid = xr.Dataset(
-    {"owiWindSpeed": result.rename({"y": "latitude", "x": "longitude"})}
-)
+out_def = output_area(7.5, 17, -60.25, -45, 0.01)
+
+file_pattern = "../../data/SAR/*/measurement/*.nc"
+
+# Loop over files of one timestep to merge them in one gridded dataset
+dss = []
+for file in glob.glob(file_pattern):
+    ds = xr.open_dataset(file)
+
+    lon, lat = (ds.owiLon, ds.owiLat)
+    in_def = GridDefinition(lons=lon.values, lats=lat.values)
+
+    resampler = XArrayResamplerNN(in_def, out_def, radius_of_influence=5000)
+    resampler.get_neighbour_info()
+    result = resampler.get_sample_from_neighbour_info(
+        ds.owiWindSpeed.rename({"owiAzSize": "y", "owiRaSize": "x"})
+    )
+
+    ds_grid = xr.Dataset(
+        {"owiWindSpeed": result.rename({"y": "latitude", "x": "longitude"})}
+    )
+
+    # Add gridded dataset
+    dss.append(ds_grid)
+
+
+# Merge all gridded datasets
+def merge(dss):
+    init_val = dss[0].load()
+    for ds in dss[1:]:
+        for var in ds.data_vars:
+            init_val[var].values[~np.isnan(ds[var])] = ds[var].values[~np.isnan(ds[var])]
+    return init_val
+
+
+ds_combined = merge(dss)
