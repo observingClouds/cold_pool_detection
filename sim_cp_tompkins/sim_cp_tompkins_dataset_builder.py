971e2dacbfd4dc432f7290ab646d631b45331b90
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-02-11 13:47:08 -0800

    WIP

---
+++ sim_cp_tompkins/sim_cp_tompkins_dataset_builder.py
@@ -0,0 +1,50 @@
+"""sim_cp_tompkins dataset."""
+
+import os
+
+import tensorflow_datasets as tfds
+
+
+class Builder(tfds.core.GeneratorBasedBuilder):
+    """DatasetBuilder for sim_cp_tompkins dataset."""
+
+    VERSION = tfds.core.Version("1.0.0")
+    RELEASE_NOTES = {
+        "1.0.0": "Initial release.",
+    }
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        return self.dataset_info_from_configs(
+            features=tfds.features.FeaturesDict({
+                "image": tfds.features.Image(),
+                "label": tfds.features.LabeledImage(labels=["environment", "coldpool"]),
+            }),
+            supervised_keys=("image", "label"),  # Set to `None` to disable
+            homepage="https://github.com/observingClouds/cold_pool_detection",
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Returns SplitGenerators."""
+        data_dir = dl_manager.download_and_extract(
+            "/Users/haukeschulz/Documents/GitHub/cold_pool_detection/data/labels.tar.gz"
+        )
+        return {
+            tfds.Split.TRAIN: self._generate_examples(path=data_dir),
+        }
+
+    def _open_file(self, path):
+        if os.path.isfile(path):
+            with open(path, "rb") as file_obj:
+                return file_obj
+
+    def _generate_examples(self, path):
+        """Yields examples."""
+        masks = path.glob("labels/*cold_pool_mask.png")
+        for filename in masks:
+            filename.parts
+            label_fn = str(filename).replace("cold_pool_mask", "satellite")
+            yield filename, {
+                "image": filename,
+                "label": label_fn,
+            }
