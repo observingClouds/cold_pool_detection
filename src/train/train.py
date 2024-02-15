70781332efa1d3a2accdfcf777749b0cf2aa4a18
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-02-15 00:58:31 -0800

    running impl. of DenseNet121

--- src/train/train.py
+++ src/train/train.py
@@ -75,13 +75,10 @@ for images, masks in train_batches.take(2):
     sample_image, sample_mask = images[0], masks[0]
     helpers.display([sample_image, sample_mask])
 
-base_model = tf.keras.applications.MobileNetV2(
-    input_shape=[128, 128, 3], include_top=False
-)
 base_model = tf.keras.applications.DenseNet121(
     include_top=False,
     weights=None,
-    input_shape=(120, 120, 3),
+    input_shape=(128, 128, 3),
     pooling="avg",
 )
 
@@ -92,14 +89,14 @@ checkpoint.restore("models/checkpoints/checkpoint_DenseNet121/checkpoints-4.inde
 
 # Use the activations of these layers
 layer_names = [
-    "block_1_expand_relu",  # 64x64
-    "block_3_expand_relu",  # 32x32
-    "block_6_expand_relu",  # 16x16
-    "block_13_expand_relu",  # 8x8
-    "block_16_project",  # 4x4
+    "conv1/relu",  # 60x60 64
+    "conv2_block6_concat",  # 30x30 256
+    "conv3_block12_concat",  # 15x15 512
+    "conv4_block24_concat",  # 7x7 1024
+    # "conv5_block16_concat",  # 3x3 1024
 ]
 
-layer_names = [layer.name for layer in base_model.layers]
+# layer_names = [layer.name for layer in base_model.layers]
 base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
 
 # Create the feature extraction model
@@ -107,10 +104,10 @@ down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
 
 down_stack.trainable = False
 up_stack = [
-    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-    pix2pix.upsample(64, 3),  # 32x32 -> 64x64
+    # pix2pix.upsample(1024, 3),  # 3x3 -> 7x7
+    pix2pix.upsample(512, 3),  # 7x7 -> 15x15
+    pix2pix.upsample(256, 3),  # 15x15 -> 30x30
+    pix2pix.upsample(128, 3),  # 30x30 -> 64x64
 ]
 
 
@@ -131,7 +128,7 @@ def unet_model(output_channels: int):
     # This is the last layer of the model
     last = tf.keras.layers.Conv2DTranspose(
         filters=output_channels, kernel_size=3, strides=2, padding="same"
-    )  # 64x64 -> 128x128
+    )
 
     x = last(x)
 
