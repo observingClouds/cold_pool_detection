762111b85f7333bb8503931430c01beb326ec0e6
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2024-03-20 21:55:54 +0000

    Reapply "change back to MobileNetV2"
    This reverts commit 67ceb0ecad71c0aa20d3ac344d7c74eefa2b81a5.

--- src/train/train.py
+++ src/train/train.py
@@ -98,23 +98,19 @@ helpers.raster_display(
     out_mask="eval/labels/sample_masks.png",
 )
 
-base_model = tf.keras.applications.DenseNet121(
-    include_top=False,
-    weights=None,
-    input_shape=(128, 128, 3),
-    pooling="avg",
+base_model = tf.keras.applications.MobileNetV2(
+    input_shape=[128, 128, 3], include_top=False
 )
 
 # Use the activations of these layers
 layer_names = [
-    "conv1_relu",  # 60x60 64
-    "conv2_block6_concat",  # 30x30 256
-    "conv3_block12_concat",  # 15x15 512
-    "conv4_block24_concat",  # 7x7 1024
-    "conv5_block16_concat",  # 3x3 1024
+    "block_1_expand_relu",  # 64x64
+    "block_3_expand_relu",  # 32x32
+    "block_6_expand_relu",  # 16x16
+    "block_13_expand_relu",  # 8x8
+    "block_16_project",  # 4x4
 ]
 
-# layer_names = [layer.name for layer in base_model.layers]
 base_model_outputs = [base_model.get_layer(name).output for name in layer_names]
 
 # Create the feature extraction model
@@ -122,10 +118,10 @@ down_stack = tf.keras.Model(inputs=base_model.input, outputs=base_model_outputs)
 
 down_stack.trainable = False
 up_stack = [
-    pix2pix.upsample(1024, 3),  # 3x3 -> 7x7
-    pix2pix.upsample(512, 3),  # 7x7 -> 15x15
-    pix2pix.upsample(256, 3),  # 15x15 -> 30x30
-    pix2pix.upsample(128, 3),  # 30x30 -> 64x64
+    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+    pix2pix.upsample(64, 3),  # 32x32 -> 64x64
 ]
 
 
@@ -146,7 +142,7 @@ def unet_model(output_channels: int):
     # This is the last layer of the model
     last = tf.keras.layers.Conv2DTranspose(
         filters=output_channels, kernel_size=3, strides=2, padding="same"
-    )
+    )  # 64x64 -> 128x128
 
     x = last(x)
 
